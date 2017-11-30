3daec61cb2ea99073e1428480c2c39128418d09e
Alihasana <hasana.ali@gmail.com>
2017-11-30 16:01:45 +0100

    good

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 var express = require('express');
 var app = express();
+var _list =require('./nba-teams.json');
+
+app.get('/teams',function(req, res){
+	var list = req.query;
+
+	res.status(200).send(_list);
+
+});
+
+app.listen(3000, function() {
+	console.log('app listening on the port!');
+});

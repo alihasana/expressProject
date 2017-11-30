ae0620ca8cb9af6cf9089c6011045a41e284fe14
HanPhoenix01 <loic.aublet@gmail.com>
2017-11-30 15:54:00 +0100

    Deuxiéme étapes

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 var express = require('express');
 var app = express();
+var list = require("./nba-teams.json");
+
+app.get('/teams', function(req, res) {
+
+	res.status(200).send(list);
+});
+
+app.get('/teams/:id', function(req, res) {
+	var _id = req.params.id;
+	
+	var teamsId = list.find(function(pro){
+		return pro.teamId == _id; });
+	var teamName = list.find(function(pro){
+		return pro.teamName == _id;});
+	var abbreviation = list.find(function(pro){
+		return pro.abbreviation == _id;});
+
+	
+
+	if (teamsId || teamName || abbreviation ) { 
+		res.status(200).send(teamsId || teamName || abbreviation);
+	} else {
+		res.status(200).send('Good Job Anas ! Keep Trying !');
+		// res.status(200).send(teamsId);
+	};
+	
+});
+
+app.listen(3000);
